3f2c5e746637f00d9360d35d7ad7d44b4277b666
AsyncBanana <jacobedwardjackson@outlook.com>
2021-08-24 10:46:24 -0400

    fixed accessibility

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -6,9 +6,9 @@
 </script>
 
 <a class="rounded-md bg-base-100 p-4" href={`/posts/${item.slug}`}>
-	<img src={item.content.image} alt="Article header" class="rounded-md"/>
+	<img src={item.content.image} alt="Article header" class="rounded-md" />
 	<h1 class="font-bold text-xl">{item.content.title}</h1>
-	<h3>{item.content.intro}</h3>
-	<h4>By {item.content.author.name}</h4>
-	<h4>{dayjs(item.published_at).calendar()}</h4>
+	<p>{item.content.intro}</p>
+	<p>By {item.content.author.name}</p>
+	<p>{dayjs(item.published_at).calendar()}</p>
 </a>
